33017510a14ade54001c0929992d2df6667774a1
Beyzanur <75632809+Beyzzz@users.noreply.github.com>
2022-11-10 14:14:32 +0300

    ArikYilHesaplamaPatika

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Scanner input = new Scanner(System.in);
+
+	        int year;
+	        System.out.print("Yil: ");
+	        year = input.nextInt();
+
+	        if(year % 4 == 0){
+	            if(year % 100 == 0 ){
+	                if(year % 400 == 0){
+	                    System.out.println(year + " artik yil.");
+	                }else System.out.println(year+"  artik yil degil.");
+	            }else System.out.println(year+" bir artik yildir.");
+	        }
+	        else System.out.println(year+" bir artik yil değildir.");
+
+	}
+
+}
